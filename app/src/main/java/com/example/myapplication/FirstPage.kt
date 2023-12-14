07d566ffaa5422023a14f859f3af0b77fb712001
na30 <r.sulfus2@gmail.com>
2023-12-14 10:46:35 -0800

    ui debug

--- app/src/main/java/com/example/myapplication/FirstPage.kt
+++ app/src/main/java/com/example/myapplication/FirstPage.kt
@@ -1,21 +1,40 @@
 package com.example.myapplication
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.myapplication.ui.theme.MyApplicationTheme
 
 @Composable
 fun FirstPage(navController: NavController) {
     // Define your data
     val name = "John"
     val lastName = "Doe"
-
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally    ){
+        Button(onClick = {
+            navController.navigate("secondPage/$name/$lastName")
+        }) {
+            Text(text = "first page btn")
+            // Your button content
+        }
+    }
     // Navigate to the second page with data
-    Button(onClick = {
-        navController.navigate("secondPage/$name/$lastName")
-    }) {
-        Text(text = "first page btn")
-        // Your button content
+
+}
+@Preview(showBackground = true)
+@Composable
+fun FirstPagePreview() {
+    MyApplicationTheme {
+
     }
 }
--- app/src/main/java/com/example/myapplication/SecondPage.kt
+++ app/src/main/java/com/example/myapplication/SecondPage.kt
@@ -1,7 +1,13 @@
 package com.example.myapplication
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 //import androidx.navigation.compose.navArgument
 
@@ -9,12 +15,17 @@ import androidx.navigation.NavController
 fun SecondPage(navController: NavController, name: String, lastName: String) {
     // Use the received data in your UI
     // ...
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+// Navigate back to the first page if needed
+        Button(onClick = { navController.popBackStack() }) {
+            // Your button content
+            Text(text = "second page btn")
 
-    // Navigate back to the first page if needed
-    Button(onClick = { navController.popBackStack() }) {
-        // Your button content
-        Text(text = "second page btn")
-
+        }
     }
 }
+
 
